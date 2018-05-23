5c419b9d12d42b86141d743d82d28adcb22858b3
ToBeAmped <martinja@oregonstate.edu>
2018-05-23 14:51:08 -0700

    Get to shameless green

--- lib/game.rb
+++ lib/game.rb
@@ -1,21 +1,48 @@
 class Game
 
-  attr_reader :started
+  attr_reader :started, :finished
 
   def start
     @started = true
+    @finished = true
   end
 
   def started?
     self.started || false
+  end
+  
+  def finished?
+    self.finished || true 
   end
 
   def play(left = nil, right = nil)
     raise 'Game must first be started' unless started?
     return nil unless (left && right)
     if (left == :rock && right == :scissors)
+      @finished = true
       "Rock beats scissors!"
-    else
+    elsif (left == :scissors && right == :rock)
+      @finished = true
+      "Rock beats scissors!"
+    elsif (left == :rock && right == :paper)
+      @finished = true
+      "Paper beats rock!"
+    elsif (left == :paper && right == :rock)
+      @finished = true
+      "Paper beats rock!"
+    elsif (left == :scissors && right == :paper)
+      @finished = true
+      "Scissors beats paper!"
+    elsif (left == :paper && right == :scissors)
+      @finished = true
+      "Scissors beats paper!"
+    elsif (left == :rock && left == right)
+      "Tie game. Try again!"
+    elsif (left == :scissors && left == right)
+      "Tie game. Try again!"
+    elsif (left == :paper && left == right)
+      "Tie game. Try again!"
+    else 
       :UNKNOWN
     end
   end
